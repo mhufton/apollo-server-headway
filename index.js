102145b7648b91a31b9a2d25f79e7091960a79fb
Mike Hufton <78620893+mhufton@users.noreply.github.com>
2022-02-28 13:08:32 -0800

    acquainted myself with graphql and apollo server

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const { ApolloServer, gql } = require("apollo-server");
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: (root, args, context) => "Hello world!"
-  }
-};
+const typeDefs = require("./schema");
+const resolvers = require("./resolver");
+const models = require("./models");
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: { models }
 });
 
 server.listen().then(({ url }) => {
